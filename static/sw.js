2d0c2021feda62f09298b92cc07405e1a2e55b92
levanchic2-lang <levanchic2@gmail.com>
2026-06-01 17:50:55 +0800

    style(客户档案): 去掉 14 个 tab 旁的数字徽章
    大部分时间都是 0，纯视觉噪音
    钱包 ¥0 / 套餐 0 / 押金 0 / 优惠券 0 占空间又无信息量
    保留干净的「诊疗 处方 检查单 预约 ...」文字标签

--- static/sw.js
+++ static/sw.js
@@ -5,7 +5,7 @@
  *   - API / 表单 POST：直通网络（不缓存）
  *   - 升级版本时改 CACHE_VERSION，自动清旧
  */
-const CACHE_VERSION = 'v145-grooming-from-inv';
+const CACHE_VERSION = 'v146-customer-tabs-clean';
 const STATIC_CACHE = 'static-' + CACHE_VERSION;
 const PAGE_CACHE   = 'pages-' + CACHE_VERSION;
 
